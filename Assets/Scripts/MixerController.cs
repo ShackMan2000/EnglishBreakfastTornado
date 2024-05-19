89f598de670da85beb50bb9010c9197b33496cd5
Julian <julianpieper@gmx.de>
2024-05-19 13:27:00 +0200

    setup

---
+++ Assets/Scripts/MixerController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class MixerController : MonoBehaviour
+{
+
+    public InputActionReference triggerInputActionRef;
+
+    public Transform hooks;
+    
+    public float rotationSpeed = 1.0f;
+
+
+    void Update()
+    {
+        float triggerValue = triggerInputActionRef.action.ReadValue<float>();
+        
+        float rotation = triggerValue * rotationSpeed * Time.deltaTime;
+        hooks.Rotate(Vector3.up, rotation);
+    }
+}
